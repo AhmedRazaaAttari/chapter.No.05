699f2dc241386e89c1d6a94ff5be9a9a774f5afb
AhmedRazaaAttari <154536166+AhmedRazaaAttari@users.noreply.github.com>
2024-03-08 00:41:47 +0500

    Add files via upload

---
+++ Chapter.No.05/app.js
@@ -0,0 +1,10 @@
+// 1. Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+
+ let num1 = +prompt("enter your number1");
+ let num2 = +prompt("enter your number2");
+
+ let sum = num1 + num2;
+ document.write(" sum of " , num1 , " and " , num2 , " is " , sum);
+
+ 
